121a9c599bc3b2a4b0f1fc911e17f2f48035af2b
Piotr <piotr.drzymkowski@idego.pl>
2026-04-24 11:09:31 +0200

    Fix typo in Settings class, add s3_url property

--- img-api/app/config.py
+++ img-api/app/config.py
@@ -4,17 +4,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env_file", env_file_encoding="utf-8")
 
-    db_url: str
+    db_url: str = ""
     aws_access_key_id: str = ""
     aws_secret_access_key: str = ""
     aws_region: str = "us-east-1"
     s3_bucket_name: str = ""
 
+    @property
+    def s3_url(self):
+        return f"https://{self.s3_bucket_name}.s3.{self.aws_region}.amazonaws.com"
 
 _settings = None
 
 def get_settings() -> Settings:
     global _settings
     if _settings is None:
-        settings = Settings()
+        _settings = Settings()
     return _settings
