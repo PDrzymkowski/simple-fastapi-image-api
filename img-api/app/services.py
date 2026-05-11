1f260c1db9b831c54788d47d90a07f0b76a0e3e1
Piotr <piotr.drzymkowski@idego.pl>
2026-05-11 10:33:23 +0200

    Stop creating S3 client globally

--- img-api/app/services.py
+++ img-api/app/services.py
@@ -1,26 +1,29 @@
 import boto3
+from functools import lru_cache
 from .config import settings
 
 __all__ = ["upload_image_to_s3", "delete_image_from_s3"]
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=settings.aws_access_key_id,
-    aws_secret_access_key=settings.aws_secret_access_key,
-    region_name=settings.aws_region,
-)
+
+@lru_cache(maxsize=1)
+def _get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=settings.aws_access_key_id,
+        aws_secret_access_key=settings.aws_secret_access_key,
+        region_name=settings.aws_region,
+    )
 
 
 def upload_image_to_s3(data: bytes, key: str, content_type: str) -> str:
-    """
-    Uploads image to AWS S3.
+    """Uploads image to AWS S3.
 
     :param data: Image data in bytes
     :param key: Key to AWS S3
     :param content_type: Type of content
     :return: Image URL in S3
     """
-    s3.put_object(
+    _get_s3_client().put_object(
         Bucket=settings.s3_bucket_name,
         Key=key,
         Body=data,
@@ -34,4 +37,4 @@ def delete_image_from_s3(key: str) -> None:
 
     :param key: Key of the object to delete.
     """
-    s3.delete_object(Bucket=settings.s3_bucket_name, Key=key)
+    _get_s3_client().delete_object(Bucket=settings.s3_bucket_name, Key=key)
