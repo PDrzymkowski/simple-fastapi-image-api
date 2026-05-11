fff15e03d1a8cc8469edfe2ffcc3551d1ab89714
Piotr <piotr.drzymkowski@idego.pl>
2026-05-11 10:34:18 +0200

    Remove default value from DB_URL

--- img-api/app/config.py
+++ img-api/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    db_url: str = ""
+    db_url: str
     aws_access_key_id: str = ""
     aws_secret_access_key: str = ""
     aws_region: str = "us-east-1"
